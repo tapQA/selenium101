353a89c8a70f1357c9e528d9f20fc2f26a5e565e
Colin Sullivan <cms.colin.sullivan@gmail.com>
2016-11-07 22:55:56 -0600

    finished example python tests

--- python/README.md
+++ python/README.md
@@ -14,9 +14,19 @@ python -V
 If you see output, you're all set.
 If you get an error, follow the Windows instructions below.
 
-##### For Windows 
+##### For Windows
 
 - Download python 3.5.x from https://www.python.org/downloads/.
 - Run the installer with all default settings.
 
 ----------------------------
+
+### Next Steps
+
+1. Clone this code repository
+2. Install python deps with pip
+3. Install a selenium server (suggestions: selenium.jar or selenium-standalone)
+4. Run the example test
+``` python
+python espn_homepage.py
+```
--- python/espn_homepage.py
+++ python/espn_homepage.py
@@ -1,9 +1,25 @@
 from selenium import webdriver
-#from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Firefox()
+caps = {'platform': 'WINDOWS', 'browserName': 'internet explorer', 'version': '11', 'javascriptEnabled': True}
+selenium_grid_url = 'http://127.0.0.1:4444/wd/hub'
+
+driver = webdriver.Remote(desired_capabilities=caps,command_executor=selenium_grid_url)
+
+# example 1
 driver.get("http://www.espn.com/esports/")
-assert "esports News: LoL, Dota, Hearthstone" in driver.title
-elem = driver.find_element_by_class("headlines")
+print ('page title is: ', driver.title)
+assert "esports News: LoL, Dota 2, Hearthstone - ESPN" in driver.title
+
+# example 2
+elem = driver.find_elements_by_css_selector('div.headlines a')
+print ('top esports headlines:')
+for i in elem:
+    print(i.text)
+
+# example 3
+driver.get('http://www.espn.com/nhl/team/_/name/min/minnesota-wild')
+elem = driver.find_element_by_css_selector('div.span-2 h4')
+print ('next MN Wild game is on:')
+print (elem.text)
 
 driver.close()
