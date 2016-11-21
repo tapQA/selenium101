7c84d1a8e62e8cab95dbc7ce45e5657d02c09947
Colin Sullivan <cms.colin.sullivan@gmail.com>
2016-11-21 17:52:01 -0600

    finished ruby examples

--- ruby/espn_homepage.rb
+++ ruby/espn_homepage.rb
@@ -4,12 +4,15 @@ driver = Selenium::WebDriver.for :chrome
 
 # example 1
 driver.navigate.to "http://www.espn.com/college-football/"
-puts "top college football headlines"
 elements = driver.find_element(:css, 'div#news-feed-content div.headlines')
-puts elements
+puts elements.text
 
 # example 2
 driver.navigate.to "http://www.espn.com/"
-driver.click(:link_text, 'NBA')
+link = driver.find_element(:link_text, 'NBA')
+link.click
+page_title = driver.title
+puts 'page title is:'
+puts page_title
 
 driver.quit
